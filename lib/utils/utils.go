262e0f2259696cb35f2d7094b23de3646dedb6cd
wangkaipeng <1592622761@qq.com>
2022-10-15 19:49:58 +0800

    临时提交

--- lib/utils/utils.go
+++ lib/utils/utils.go
@@ -1,6 +1,6 @@
 package utils
 
-// ToCmdLine convert strings to [][]byte
+// ToCmdLine 将字符串转换为[][]byte
 func ToCmdLine(cmd ...string) [][]byte {
 	args := make([][]byte, len(cmd))
 	for i, s := range cmd {
@@ -9,7 +9,7 @@ func ToCmdLine(cmd ...string) [][]byte {
 	return args
 }
 
-// ToCmdLine2 convert commandName and []byte-type argument to CmdLine
+// ToCmdLine2 将commandName和[]byte类型参数转换为CmdLine
 func ToCmdLine2(commandName string, args ...[]byte) [][]byte {
 	result := make([][]byte, len(args)+1)
 	result[0] = []byte(commandName)
@@ -19,7 +19,7 @@ func ToCmdLine2(commandName string, args ...[]byte) [][]byte {
 	return result
 }
 
-// BytesEquals check whether the given bytes is equal
+// BytesEquals 检查给定byte是否相等
 func BytesEquals(a []byte, b []byte) bool {
 	if (a == nil && b != nil) || (a != nil && b == nil) {
 		return false
