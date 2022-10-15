0f58e388dda48dbf8d025616fc82624fd4fe3ab5
wangkaipeng <1592622761@qq.com>
2022-10-15 19:39:47 +0800

    临时提交

--- interface/tcp/handler.go
+++ interface/tcp/handler.go
@@ -5,10 +5,10 @@ import (
 	"net"
 )
 
-// HandleFunc represents application handler function
+// HandleFunc 表示程序处理程序函数
 type HandleFunc func(ctx context.Context, conn net.Conn)
 
-// Handler represents application handler over tcp
+// Handler 表示tcp上的程序处理程序
 type Handler interface {
 	Handle(ctx context.Context, conn net.Conn)
 	Close() error
