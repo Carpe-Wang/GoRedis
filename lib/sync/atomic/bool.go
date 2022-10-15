b792f4b9828a4893e719e4c1425b4424e5c36f2d
wangkaipeng <1592622761@qq.com>
2022-10-15 19:47:15 +0800

    临时提交

--- lib/sync/atomic/bool.go
+++ lib/sync/atomic/bool.go
@@ -2,15 +2,15 @@ package atomic
 
 import "sync/atomic"
 
-// Boolean is a boolean value, all actions of it is atomic
+// Boolean 是一个布尔值，它的所有动作都是原子的
 type Boolean uint32
 
-// Get reads the value atomically
+// Get 自动读取值
 func (b *Boolean) Get() bool {
 	return atomic.LoadUint32((*uint32)(b)) != 0
 }
 
-// Set writes the value atomically
+// Set 以原子方式写入值
 func (b *Boolean) Set(v bool) {
 	if v {
 		atomic.StoreUint32((*uint32)(b), 1)
