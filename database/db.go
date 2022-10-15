de2ff86833654fc18771a68d2ac5ccf9f3dcd727
wangkaipeng <1592622761@qq.com>
2022-10-15 19:26:00 +0800

    修改一些细节

--- database/db.go
+++ database/db.go
@@ -56,7 +56,6 @@ func validateArity(arity int, cmdArgs [][]byte) bool {
 
 /* ---- 连接数据库 ----- */
 
-// GetEntity returns DataEntity bind to given key
 func (db *DB) GetEntity(key string) (*database.DataEntity, bool) {
 
 	raw, ok := db.data.Get(key)
@@ -67,27 +66,24 @@ func (db *DB) GetEntity(key string) (*database.DataEntity, bool) {
 	return entity, true
 }
 
-// PutEntity a DataEntity into DB
 func (db *DB) PutEntity(key string, entity *database.DataEntity) int {
 	return db.data.Put(key, entity)
 }
 
-// PutIfExists edit an existing DataEntity
 func (db *DB) PutIfExists(key string, entity *database.DataEntity) int {
 	return db.data.PutIfExists(key, entity)
 }
 
-// PutIfAbsent insert an DataEntity only if the key not exists
 func (db *DB) PutIfAbsent(key string, entity *database.DataEntity) int {
 	return db.data.PutIfAbsent(key, entity)
 }
 
-// Remove the given key from db
+// Remove 指定的key清除
 func (db *DB) Remove(key string) {
 	db.data.Remove(key)
 }
 
-// Removes the given keys from db
+// Removes 根据key，清除数据库
 func (db *DB) Removes(keys ...string) (deleted int) {
 	deleted = 0
 	for _, key := range keys {
@@ -100,7 +96,6 @@ func (db *DB) Removes(keys ...string) (deleted int) {
 	return deleted
 }
 
-// Flush clean database
 func (db *DB) Flush() {
 	db.data.Clear()
 }
